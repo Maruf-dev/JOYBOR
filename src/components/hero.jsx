cce238d7429ca467400276a93103e14ad6e40fb6
maruf-dev <maruf123erkinov@gmail.com>
2024-11-16 18:27:36 +0500

    removed bold font

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -16,7 +16,7 @@ const Hero = ({ address, phone, email }) => {
     <section className="flex flex-col w-full h-screen bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)]">
       <div className="w-full h-screen relative">
         <div className="relative h-screen text-center flex flex-col">
-          <h1 className="font-[500] text-7xl text-center mt-36 font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text capitalize">
+          <h1 className="font-[500] text-7xl text-center mt-36 tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text capitalize">
             Joyborda <br />joy bor!
           </h1>
         </div>
